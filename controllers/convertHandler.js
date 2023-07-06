84fb95e50d1ef8380ff10016626f3c41ed7a82d1
user <user@gmail.com>
2023-07-06 20:17:22 +0300

    WIP convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,51 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+  this.getNum = (input) => {
+    let result = input.match(/\d+/)[0]
+    return result
+  }
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
+  this.getUnit = (input) => {
+    let result = input.match(/[A-Za-z]+/)[0].toLowerCase()
+
+    return result
+  }
+
+  this.getReturnUnit = (initUnit) => {
+    // const galToL = 3.78541, lbsToKg = 0.453592, miToKm = 1.60934
+    const units = { gal: 'L', l: 'gal', lbs: 'kg', kg: 'lbs', mi: 'km', km: 'mi' }
+    let result
+    for (const item in units) {
+      if (item == initUnit) {
+        result = item
+      }
+    }
+    console.log(result)
+
+    return result
+  }
+
+  this.spellOutUnit = (unit) => {
+    let result
+
+    return result
+  }
+
+  this.convert = (initNum, initUnit) => {
+    const galToL = 3.78541
+    const lbsToKg = 0.453592
+    const miToKm = 1.60934
+
+    // let result = initNum
+    // console.log(result)
+
+    return result
+  }
+
+  this.getString = (initNum, initUnit, returnNum, returnUnit) => {
+    let result
+
+    return result
+  }
 }
 
-module.exports = ConvertHandler;
+module.exports = ConvertHandler
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,19 @@
-'use strict';
+'use strict'
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const expect = require('chai').expect
+const ConvertHandler = require('../controllers/convertHandler.js')
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
+  let convertHandler = new ConvertHandler()
+  app.route('/api/convert').get(function (req, res) {
+    const initNum = convertHandler.getNum(req.query.input)
+    const initUnit = convertHandler.getUnit(req.query.input)
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    // const returnNum = convertHandler.spellOutUnit(initNum, initUnit)
+    // const returnNum = convertHandler.convert(initNum, initUnit)
 
-};
+    // const result = convertHandler.getReturnUnit(initNum, initUnit, returnNum, returnUnit)
+    // console.log(result)
+    res.json('ok')
+  })
+}
