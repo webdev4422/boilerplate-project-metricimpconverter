b5a443383f1367a93456b50d8b51665c251df961
user <user@gmail.com>
2023-07-08 16:42:28 +0300

    Handle 'L' case

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -15,13 +15,14 @@ function ConvertHandler() {
     result = result[0].toLowerCase()
     for (let i = 0; i < units.length; i++) {
       if (result == units[i]) {
+        if (result == 'l') return result.toUpperCase() // Handle 'L'
         return result // initUnit
       }
     }
   }
 
   this.getReturnUnit = (initUnit) => {
-    const unitsOpposite = { gal: 'L', l: 'gal', lbs: 'kg', kg: 'lbs', mi: 'km', km: 'mi' }
+    const unitsOpposite = { gal: 'L', L: 'gal', lbs: 'kg', kg: 'lbs', mi: 'km', km: 'mi' }
     let result
 
     for (const item in unitsOpposite) {
@@ -34,7 +35,7 @@ function ConvertHandler() {
 
   this.spellOutUnit = (unit) => {
     //prettier-ignore
-    const unitsSpellOut = { gal: 'gallons ', l: 'liters', lbs: 'pounds', kg: 'kilograms', mi: 'miles', km: 'kilometers ', }
+    const unitsSpellOut = { gal: 'gallons ', L: 'liters', lbs: 'pounds', kg: 'kilograms', mi: 'miles', km: 'kilometers ', }
     let result
 
     for (const item in unitsSpellOut) {
@@ -47,7 +48,7 @@ function ConvertHandler() {
 
   this.convert = (initNum, initUnit) => {
     //prettier-ignore
-    const unitsValues = { gal: 3.78541, l: 0.264172176857989, lbs: 0.453592, kg: 2.204624420183777, mi: 1.60934, km: 0.6213727366498068, }
+    const unitsValues = { gal: 3.78541, L: 0.264172176857989, lbs: 0.453592, kg: 2.204624420183777, mi: 1.60934, km: 0.6213727366498068, }
     const galToL = 3.78541
     const lbsToKg = 0.453592
     const miToKm = 1.60934
