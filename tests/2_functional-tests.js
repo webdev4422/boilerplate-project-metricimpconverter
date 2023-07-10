447148dfcaa36cc10ec16380a03cfbbb1b9c3b17
user <user@gmail.com>
2023-07-10 13:51:48 +0300

    Set integration tests with chai-http

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,35 @@
-const chaiHttp = require('chai-http');
-const chai = require('chai');
-let assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require('chai-http')
+const chai = require('chai')
+let assert = chai.assert
+const server = require('../server')
+let expect = require('chai').expect
+chai.use(chaiHttp)
 
-chai.use(chaiHttp);
-
-suite('Functional Tests', function() {
-
-});
+suite('Functional Tests', () => {
+  suite('Integration tests with chai-http', () => {
+    test('#1 Convert a valid input such as 10L: GET request to /api/convert.', (done) => {
+      chai
+        .request(server)
+        .keepOpen()
+        .get('/api/convert')
+        .query({ input: '10L' }) // /api/convert/?input=10L
+        .end((err, res) => {
+          expect(err).to.be.null
+          expect(res).to.have.status(200)
+          done()
+        })
+    })
+    // test('#2 Convert an invalid input such as 32g: GET request to /api/convert.', () => {
+    //   // expect(convertHandler.getReturnUnit('L')).to.be.equal('gal')
+    // })
+    // test('#3 Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.', () => {
+    //   // expect(convertHandler.getReturnUnit('mi')).to.be.equal('km')
+    // })
+    // test('#4 Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.', () => {
+    //   // expect(convertHandler.getReturnUnit('km')).to.be.equal('mi')
+    // })
+    // test('#5 Convert with no number such as kg: GET request to /api/convert.', () => {
+    //   // expect(convertHandler.getReturnUnit('lbs')).to.be.equal('kg')
+    // })
+  })
+})
